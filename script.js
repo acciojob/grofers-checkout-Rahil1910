d99ec101edfcc9c99691beb96d130209f786baa7
Rahil1910 <rahilrafique1234@gmail.com>
2025-01-01 11:19:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,24 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
-};
+	let sum1=0;
+    const prices = document.querySelectorAll('.price');
+	prices.forEach(function(price) {
+		sum1 += parseFloat(price.textContent); 
+	});
 
+	const table = document.querySelector('table');
+	const row = document.createElement('tr');
+
+	const cell1 = document.createElement('td');
+	cell1.textContent = 'Total Price';
+	row.appendChild(cell1);
+	
+	const cell2 = document.createElement('td');
+	cell2.textContent = sum1;
+	row.appendChild(cell2);
+
+	table.appendChild(row);
+};
 getSumBtn.addEventListener("click", getSum);
 

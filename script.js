66ac48f97ee7662b41e7445aa1bb1ceb2d81fbae
Rahil1910 <rahilrafique1234@gmail.com>
2025-01-01 11:25:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,12 +11,14 @@ const getSum = () => {
 
 	const table = document.querySelector('table');
 	const row = document.createElement('tr');
-
+	
 	const cell1 = document.createElement('td');
+	cell1.id = 'ans';
 	cell1.textContent = 'Total Price';
 	row.appendChild(cell1);
 	
 	const cell2 = document.createElement('td');
+	cell2.id = 'ans';
 	cell2.textContent = sum1;
 	row.appendChild(cell2);
 
